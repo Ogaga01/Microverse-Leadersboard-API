794b5ac79cb212611a03a5dc3e61556d93369979
Ogaga <iyaraogaga69@gmail.com>
2022-10-25 11:38:34 +0100

    Add addScore module

---
+++ src/modules/addScore.js
@@ -0,0 +1,15 @@
+import { fetchScores, postScores } from "./API";
+
+const nameInput = document.querySelector('.name-input')
+const scoreInput = document.querySelector('.score-input')
+const addScore = document.querySelector('.add')
+
+const scoreAdd = () => {
+    addScore.addEventListener("click", (name, score) => {
+      name = nameInput.value;
+      score = scoreInput.value;
+      postScores(name, score);
+      fetchScores();
+    });
+}
+export {scoreAdd}
